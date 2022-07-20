02207c258f4ffbc1ee3f13b5da50995df9ee71c6
Emilce Robles <emi.nrobles@gmail.com>
2022-07-20 00:36:00 -0300

    Actualización

--- main.js
+++ main.js
@@ -1,55 +1,91 @@
 //Declaracion de variables
 let verduras = [
     {
-        verdura: 'Tomate',
+        nombre: 'Tomate',
         tipo: 'Redondo',
         peso: '1 kg',
-        precio: 200,
+        precio: '200$',
         imagen: 'img/tomate1.jpg'        
     },
     {
-        verdura: 'Cebolla',
+        nombre: 'Cebolla',
         tipo: 'Común',
         peso: '1 kg',
-        precio: 180,
+        precio: '180$',
         imagen: 'img/cebolla.jpg'
     },
     {
-        verdura: 'Lechuga',
+        nombre: 'Lechuga',
         tipo: 'Mantecosa',
         peso: '1 kg',
-        precio: 170,
+        precio: '170$',
         imagen: 'img/lechuga.jpg'
     },
     {
-        verdura: 'Papa',
+        nombre: 'Papa',
         tipo: 'Lavada',
         peso: '1 kg',
-        precio: 150,
+        precio: '150$',
         imagen: 'img/papas.jpg'
     },
     {
-        verdura: 'Pimiento',
+        nombre: 'Pimiento',
         tipo: 'Rojo',
         peso: '100 gr',
-        precio: 80,
+        precio: '80$',
         imagen: 'img/pimiento.jpg'
     },
     {
-        verdura: 'Remolacha',
+        nombre: 'Remolacha',
         tipo: 'Común',
         peso: '1 kg',
-        precio: 200,
+        precio: '200$',
         imagen: 'img/remolacha.jpg'
     },
     {
-        verdura: 'Zanahoria',
-        tipo: 'Comúno',
+        nombre: 'Zanahoria',
+        tipo: 'Danvers',
         peso: '1 kg',
-        precio: 190,
-        imagen: 'img/zanahoria.jpg'
+        precio: '190$',
+        imagen: 'img/zanahorias.jpg'
+    },
+    {
+        nombre: 'Palta',
+        tipo: 'Hass',
+        peso: 'Por unidad',
+        precio: '100$',
+        imagen: 'img/palta1.jpg'
+    },
+    {
+        nombre: 'Choclo',
+        tipo: 'Dentado',
+        peso: '1 kg',
+        precio: '250$',
+        imagen: 'img/choclo.jpg'
+    },
+    {
+        nombre: 'Zapallo',
+        tipo: 'Brasilero',
+        peso: '1 kg',
+        precio: '230$',
+        imagen: 'img/zapallo.png'
     },
 ]
 
+//Interacción con el DOM
 let miLista = document.getElementById("listaDeVerduras");
+
+
+//Buble iterador que arma cada ficha de verdura, siendo visible en el HTML
+for (let verdura of verduras){
+    miLista.innerHTML += `
+        <div class = "div-padre">
+            <h3>${verdura.nombre}</h3>
+            <p>${verdura.tipo}</p>
+            <p>${verdura.peso}</p>
+            <p>${verdura.precio}</p>
+            <div class="imagen"><img src="${verdura.imagen}"></div>
+        </div>
+    `
+}
 
