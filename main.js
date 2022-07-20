b062c44e6fec72243f78d6a2a157434cde11c1e5
Emilce Robles <emi.nrobles@gmail.com>
2022-07-19 23:51:19 -0300

    Inicial

---
+++ main.js
@@ -0,0 +1,55 @@
+//Declaracion de variables
+let verduras = [
+    {
+        verdura: 'Tomate',
+        tipo: 'Redondo',
+        peso: '1 kg',
+        precio: 200,
+        imagen: 'img/tomate1.jpg'        
+    },
+    {
+        verdura: 'Cebolla',
+        tipo: 'Común',
+        peso: '1 kg',
+        precio: 180,
+        imagen: 'img/cebolla.jpg'
+    },
+    {
+        verdura: 'Lechuga',
+        tipo: 'Mantecosa',
+        peso: '1 kg',
+        precio: 170,
+        imagen: 'img/lechuga.jpg'
+    },
+    {
+        verdura: 'Papa',
+        tipo: 'Lavada',
+        peso: '1 kg',
+        precio: 150,
+        imagen: 'img/papas.jpg'
+    },
+    {
+        verdura: 'Pimiento',
+        tipo: 'Rojo',
+        peso: '100 gr',
+        precio: 80,
+        imagen: 'img/pimiento.jpg'
+    },
+    {
+        verdura: 'Remolacha',
+        tipo: 'Común',
+        peso: '1 kg',
+        precio: 200,
+        imagen: 'img/remolacha.jpg'
+    },
+    {
+        verdura: 'Zanahoria',
+        tipo: 'Comúno',
+        peso: '1 kg',
+        precio: 190,
+        imagen: 'img/zanahoria.jpg'
+    },
+]
+
+let miLista = document.getElementById("listaDeVerduras");
+
